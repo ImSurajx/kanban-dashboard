f9b5ea062703eaac9a8058a173e86a3b961b0fbd
ImSurajx <surajxcode@gmail.com>
2026-03-30 22:52:53 +0530

    feat(logic): build drag and drop system using dragstart, dragover, and drop events
    - understood drag event lifecycle
    - used global state to track dragged element
    - removed hardcoded container conditions
    - implemented dynamic element movement using appendChilder, and drop events
    
    Signed-off-by: ImSurajx <surajxcode@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,35 +1,54 @@
-// get all card which is currently present in the ui
-let cards = document.querySelectorAll(".card");
-let containers = document.querySelectorAll(".btm");
-// store current dragable element 
-let dragElement = null;
+// select all card which is present
+const cards = document.querySelectorAll('.card');
+// select all the card container
+const containers = document.querySelectorAll('.btm');
 
-cards.forEach(element => {
-    element.addEventListener("dragstart", (e) => {
-        dragElement = element;
-        // track the parent of current drag element..
-        const originalContainer = e.target.parentElement;
-        containers.forEach(cont => {
-            cont.addEventListener("drop", (evt) => {
-                evt.preventDefault();
-                console.log(element);
-                console.log(cont);
-                if (cont.id === originalContainer.id) return;
-                else {
-                    if (cont.id === "todo-list") {
-                        element.remove();
-                        cont.appendChild(element);
-                    }
-                }
-            })
-        })
+// current moving element (global trak);
+let movingCard = null;
+let originalContainer = null;
+
+cards.forEach(card => {
+    card.addEventListener("dragstart", (e) => {
+        movingCard = card;
+        originalContainer = e.target.parentElement;
+    })
+})
+
+// we will see in future
+// container.addEventListener("dragenter", () => {
+//     container.classList.add('active');
+//     console.log(container);
+//     console.log("element enter");    
+// })
+
+// container.addEventListener("dragleave", () => {
+//     container.classList.remove('active');
+//     console.log("element remove");
+// })
+containers.forEach(container => {
+    container.addEventListener("dragover", (e) => {
+        e.preventDefault();
+    })
+    container.addEventListener('drop', (e) => {
+        e.preventDefault();
+        if (originalContainer.id === e.currentTarget.id) return;
+        else {
+            e.currentTarget.appendChild(movingCard);
+            movingCard = null;
+            // if (e.currentTarget.id === 'todo-list') {
+            //     e.currentTarget.appendChild(movingCard);
+            // }
+            // else if (e.currentTarget.id === 'in-progress-list') {
+            //     e.currentTarget.appendChild(movingCard);
+            // } else if (e.currentTarget.id === 'completed-list') {
+            //     e.currentTarget.appendChild(movingCard);
+            // }
+        }
+        // console.log(movingCard);
+        // console.log(originalContainer.id);
+        // console.log(e.currentTarget);
+        // console.log(e);
     })
 })
 
 
-
-// if (dragElement) {
-//     dragElement.addEventListener("drag", (evt) => {
-//         console.log(evt);
-//     })
-// }
